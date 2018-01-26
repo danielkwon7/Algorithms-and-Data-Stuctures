645a7968b64aa974f55ff530833c7d873459f697
Daniel <dkwonism@gmail.com>
2018-01-26 14:06:53 -0500

    Solve ownership iteratively using while loop

--- ownership.js
+++ ownership.js
@@ -46,3 +46,55 @@ var ownership = function(array) {
 
 console.log(ownership(ownership2));
 
+var ownershipIterative = function(array) {
+  var object = {};
+  var children = {};
+  var root;
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    var parent = array[i][0];
+    var child = array[i][1];
+    children[child] = true;
+    if (!object[parent]) {
+      object[parent] = [child];
+    } else {
+      object[parent].push(child);
+    }
+  }
+  Object.keys(object).forEach(function(node) {
+    if (!children[node]) {
+      root = node;
+    }
+  })
+  var stack = [[root, 0]];
+  while (stack.length) {
+    var length = stack.length;
+    var tuple = stack[length - 1];
+    var node = tuple[0];
+
+    if (tuple[2] === undefined) {
+      result.push(tuple.slice());
+    }
+
+    if (!object[node]) {
+      stack.pop();
+      continue;
+    }
+    if (tuple[2] === undefined) {
+      tuple[2] = 0;
+    }
+
+    var children = object[node];
+    var index = tuple[2];
+    if (children.length === index) {
+      stack.pop();
+      continue;
+    }
+    tuple[2]++;
+    var child = children[index];
+    var depth = tuple[1] + 1;
+    stack.push([child, depth]);
+  }
+  return result;
+}
+
