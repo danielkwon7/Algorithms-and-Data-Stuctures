1ac7fa474f9897aaab0bb5e347186c38bbeb763e
Daniel <dkwonism@gmail.com>
2018-01-25 20:08:35 -0500

    Increase efficiency by using modulos and sort

--- coinSums.js
+++ coinSums.js
@@ -1,16 +1,14 @@
 var coinSums = function(total, coins) {
   var counter = 0;
-  var coins = coins || [200, 100, 50, 20, 10, 5, 2, 1];
+  var coins = coins.sort() || [1, 2, 5, 10, 20, 50, 100, 200];
 
   var innerFunction = function(index, remainder) {
-    if (remainder === 0) {
-      counter++;
-      return;
-    } else if (remainder < 0 || index < 0) {
+    var coin = coins[index];
+    if (index === 0) {
+      remainder % coin === 0 && counter++;
       return;
     }
     while (remainder >= 0) {
-      var coin = coins[index];
       innerFunction(index - 1, remainder);
       remainder -= coin;
     }
