0639be57978e42cddafe436822f6e04365c3462c
Daniel <dkwonism@gmail.com>
2018-02-13 17:52:32 -0500

    Complete tests for numRabbits

--- rabbits.js
+++ rabbits.js
@@ -2,24 +2,47 @@
 
 // Return the minimum number of rabbits that could be in the forest.
 
+
+
 var rabbits = function(answers) {
   var result = 0;
   var carrot = {};
   for (var i = 0; i < answers.length; i++) {
-    if (carrot[i]) {
-      carrot[i]++;
+    var answer = answers[i];
+    if (carrot[answer]) {
+      carrot[answer]++;
     } else {
-      carrot[i] = 1;
+      carrot[answer] = 1;
     }
   }
+  console.log('THIS IS THE CARROT', carrot);
   for (var key in carrot) {
     var expected = Number(key) + 1;
     var count = carrot[key];
     if (expected >= count) {
       result += expected;
+      console.log('THIS IS THE RESULT', result);
     } else {
-      result += expected * ( Math.ceiling(count / expected) );
+      result += ( expected * ( Math.ceiling(count / expected) ) );
     }
   }
   return result;
 }
+
+var answers1 = [1, 1, 2];
+var results1 = 5;
+
+var answers2 = [10, 10, 10];
+var results2 = 11;
+
+var answers3 = [1, 1, 1, 1];
+var results3 = 4;
+
+console.log(rabbits(answers1));
+console.log(rabbits(answers2));
+console.log(rabbits(answers3));
+console.log(rabbits(answers1) === results1 ? 'PASS' : 'GG');
+console.log(rabbits(answers2) === results2 ? 'PASS' : 'GG');
+console.log(rabbits(answers3) === results3 ? 'PASS' : 'GG');
+
+
