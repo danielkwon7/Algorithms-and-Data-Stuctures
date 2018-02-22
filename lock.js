9d3e5d730acd4004e225898a69bbcbf5b381b096
Daniel <dkwonism@gmail.com>
2018-02-22 14:48:53 +0900

    lock working progress

---
+++ lock.js
@@ -0,0 +1,40 @@
+//to solve the shortest path problem, we use a breadth-first search
+var Queue = function() {
+  this.array = [];
+  this.start = null;
+  this.end = null;
+  this.length = 0;
+}
+
+Queue.prototype.enqueue = function(value) {
+  if (this.length === 0) {
+    this.start = 0;
+    this.end = 0;
+  }
+}
+
+var next = function(number, combination) {
+  var result = [];
+  var nextNumber = {};
+  var prevNumber = {};
+  for (var i = 0; i < 9; i++) {
+    nextNumber[i] = i + 1;
+    prevNumber[i] = i - 1;
+  }
+  nextNumber[9] = 0;
+  prevNumber[0] = 9;
+  prevNumber[9] = 1;
+  for (var i = 0; i < combination.length; i++) {
+    var number = combination[i];
+    var next = nextNumber[number];
+    var prev = prevNumber[number];
+    result.push(combination.slice().splice(i, next));
+    result.push(combination.slice().splice(i, prev));
+  }
+  return result;
+}
+
+var openLock = function(deadends, target) {
+  var queue = [];
+
+}
